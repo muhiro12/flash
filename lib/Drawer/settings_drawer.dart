c78f716610ca6a27c8a0dd0a89b01bf91cb60bb1
Hiromu Nakano <hiromu.nakano.12@gmail.com>
2020-03-07 14:38:49 +0900

    Change logic for fetching app name/version to via PackageInfo

--- lib/Drawer/settings_drawer.dart
+++ lib/Drawer/settings_drawer.dart
@@ -3,11 +3,20 @@ import 'package:flash/Drawer/color_theme_selector.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:package_info/package_info.dart';
 
-class SettingsDrawer extends StatelessWidget {
+class SettingsDrawer extends StatefulWidget {
   SettingsDrawer(this._title);
 
   final String _title;
+
+  @override
+  _SettingsDrawerState createState() => _SettingsDrawerState();
+}
+
+class _SettingsDrawerState extends State<SettingsDrawer> {
+  String _name;
+  String _version;
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +32,7 @@ class SettingsDrawer extends StatelessWidget {
                   alignment: Alignment.centerRight,
                 ),
                 Text(
-                  _title,
+                  widget._title,
                   style: Theme.of(context).textTheme.headline2,
                 ),
               ],
@@ -41,18 +50,33 @@ class SettingsDrawer extends StatelessWidget {
             title: ColorThemeSelector(),
           ),
           AboutListTile(
-            applicationVersion: '1.0.0',
+            applicationName: _name,
+            applicationVersion: _version,
             applicationIcon: SvgPicture.asset(
               'assets/images/launcher.svg',
               width: 50,
             ),
             aboutBoxChildren: <Widget>[
-              Text('Thenk you for using!!'),
+              Text('Thenk you for using Flash!!'),
             ],
             child: Text('About'),
           ),
         ],
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _setUpPackageInfo();
+  }
+
+  void _setUpPackageInfo() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    setState(() {
+      _name = packageInfo.appName;
+      _version = packageInfo.version;
+    });
   }
 }
